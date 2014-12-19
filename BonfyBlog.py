cd85517439f983284e34822bf9a8eeaa9252297a
bonfy <foreverbonfy@163.com>
2014-12-19 23:38:27 +0800

    second push

--- BonfyBlog.py
+++ BonfyBlog.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = "never_tell_you"
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/hello')
+def html_hello():
+    return "hello from bonfy"
 
 if __name__ == '__main__':
     app.run(port=8888, debug=True)
